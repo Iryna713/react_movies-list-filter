8d1d0172652578e8227bf5be7bab2e259bd69dfa
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-20 17:13:42 +0200

    Updates

---
+++ precache-manifest.e7d55e1ba3eddf067f1ef9ebc6cf6833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0ad79d0c6fda7c523440aa9a718fd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4354648fe83b41bcd55",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "7623dc2690e255c13afb",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "e4354648fe83b41bcd55",
+    "url": "./static/js/2.a8c84c83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a8c84c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7623dc2690e255c13afb",
+    "url": "./static/js/main.e6b7dd1c.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
